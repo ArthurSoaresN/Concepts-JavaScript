23a3e145420fa3138b00a0533a811e98b9c1ae5d
Arthur Soares <arthursoaressi@hotmail.com>
2026-01-19 17:00:56 -0300

    Create ambiente.js

---
+++ a17/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5,6,4]
+
+console.log(num)
+
+num[3] = 8
+num.push(7) // adicionando elemento no final
+
+console.log(num)
+
+let tamanho = num.length // tamanho do vetor
+console.log(tamanho)
+num.sort() // ordenando vetor
+console.log(num)
